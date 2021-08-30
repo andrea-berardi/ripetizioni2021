42af188396d69f6ce06699d054c6a0c0a79e7aed
Andrea Berardi <Kotheos@protonmail.com>
2021-08-30 18:04:49 +0200

    implementato es6

---
+++ esercizi_libro/pag302/es06.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define N 3 // numero di oggetti, dato dall'esercizio
+
+int main(void) {
+    float peso[N];
+
+    for (size_t i = 0; i < N; ++i) {
+        printf("Peso del %zu° oggetto: ", i + 1);
+        scanf("%f", &peso[i]);
+    }
+    printf("\n");
+
+    float tot = 0.0; // totale pesi
+    for (size_t i = 0; i < N; ++i)
+        tot += peso[i];
+    printf("Peso medio: %.2fKg\n", tot / N);
+
+    return 0;
+}

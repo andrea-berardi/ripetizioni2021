632bfd382fa92d68f99ea6c18fac8973610f60d3
Andrea Berardi <Kotheos@protonmail.com>
2021-08-30 19:20:45 +0200

    implementato es 9 reso più astratto, tanto per

---
+++ esercizi_libro/pag303/es09.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct {
+    float x;
+    float y;
+} Punto;
+
+int main(void) {
+    Punto A, B, M;
+
+    printf("Coordinata X di A: ");
+    scanf("%f", &A.x);
+    printf("Coordinata Y di A: ");
+    scanf("%f", &A.y);
+    printf("Coordinata X di B: ");
+    scanf("%f", &B.x);
+    printf("Coordinata Y di B: ");
+    scanf("%f", &B.y);
+
+    M.x = (A.x + B.y) / 2.0;
+    M.y = (A.x + B.y) / 2.0;
+
+    printf("Coordinate punto medio M(%.1f, %.1f)\n", M.x, M.y);
+
+    return 0;
+}

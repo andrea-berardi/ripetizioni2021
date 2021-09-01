78c7fa12ccf583beadd657746adb00c39fb12b75
Andrea Berardi <Kotheos@protonmail.com>
2021-09-01 18:29:37 +0200

    impl. es 3

---
+++ esercizi_libro/pag302/es03.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    float r;
+    printf("Raggio del cerchio: ");
+    scanf("%f", &r);
+    printf("C = %.2f\nA = %.2f\n", 2.0 * r * M_PI, r * r * M_PI);
+
+    return 0;
+}

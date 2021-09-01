058123c44fee9523d0c9fa487bd95b575b01dd41
Andrea Berardi <Kotheos@protonmail.com>
2021-09-01 18:12:04 +0200

    impl. es.2

---
+++ esercizi_libro/pag302/es02.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+const float φ = 2.598; // numero fisso dell'area dell'esagono (φ, Phi)
+
+int main(void) {
+    float l;
+    printf("Lato dell'esagono: ");
+    scanf("%f", &l);
+    printf("2p = %.1f\nA = %.1f\n", l * 6, l * l * φ);
+
+    return 0;
+}

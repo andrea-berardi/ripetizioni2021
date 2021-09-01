75e296fdf039f920935d94b8e5851b56b0310b7b
Andrea Berardi <Kotheos@protonmail.com>
2021-09-01 23:38:32 +0200

    impl es 5

---
+++ esercizi_libro/pag302/es05.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    float a, b, c;
+    printf("Inserisci i lati del triangolo: ");
+    scanf("%f%f%f", &a, &b, &c);
+
+    float p = (a + b + c) / 2.0; // semiperimetro
+
+    printf("Area: %.1f\n", sqrt(p * (p - a) * (p - b) * (p - c)));
+
+    return 0;
+}
